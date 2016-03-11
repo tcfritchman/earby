740c563bde69070a392380240942c4619e0eb37d
Thomas Fritchman <tcfritchman@gmail.com>
2016-03-11 09:42:23 -0800

    Fix paths in test server

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ var express = require('express');
 var app = express();
 var port = process.argv[2];
 
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/css', express.static(__dirname + '/css'));
+app.use('/', express.static(__dirname + '/build'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.htm');
+    res.sendFile(__dirname + '/build/index.htm');
 });
 
 var server = app.listen(port, function() {
-    console.log('Server listening on port ' + port)
+    console.log('Server listening on port ' + port);
 });
