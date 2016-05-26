32eb313b3266007e010f9e01125efc5de37f37f9
Thomas Fritchman <tcfritchman@gmail.com>
2016-05-26 00:29:27 -0700

    Remove old files

--- js/document.js
+++
@@ -1,7 +0,0 @@
-function loadHelp() {
-    $.get('templates/helpPage.stache', function(template) {
-        var rendered = Mustache.render(template);
-        $('#document-modal-body').html(rendered);
-    });
-    $('#document-title').html('Help');
-}
